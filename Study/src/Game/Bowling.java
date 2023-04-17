252a92f7ba07ee3febb1c65b0c6c33887cab1f04
dwng3 <answer3dw@gmail.com>
2023-04-17 19:34:25 +0900

    야구게임 커밋

---
+++ Study/src/Game/Bowling.java
@@ -0,0 +1,22 @@
+package Game;
+
+// 볼링게임을 만들어보자
+public class Bowling {
+
+    // 볼링게임을 실행할 메인메소드
+    public static void main(String[] args) {
+
+        int[] numbers = {1,2,3,4,5};
+
+        for(int currentIndex=0;currentIndex<numbers.length;currentIndex++){
+            System.out.println(currentIndex);
+        }
+
+        // 볼링 점수판 만들기
+        // 볼링 점수 계산 메소드를 만들자
+        // 랜덤함수를 사용해서 스캐너로 입력하면 점수가 계산되어야함
+        // 스트라이크와 스페어일 때 점수 계산 하는방법
+
+    }
+}
+
---
+++ Study/src/SoloJava/WriteExample.java
@@ -0,0 +1,13 @@
+package SoloJava;
+
+import java.util.Scanner;
+
+public class WriteExample {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String str =  sc.next();
+        System.out.println("엉덩이" + str);
+
+    }
+}
