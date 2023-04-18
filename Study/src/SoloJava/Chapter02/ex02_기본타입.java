0abd52b01d3b45eca0b05c68b51046e5ab0f8951
dwng3 <answer3dw@gmail.com>
2023-04-18 12:36:50 +0900

    혼자공부하는 자바 커밋

---
+++ Study/src/SoloJava/Chapter02/ex02_기본타입.java
@@ -0,0 +1,20 @@
+package SoloJava.Chapter02;
+
+public class ex02_기본타입 {
+    public static void main(String[] args) {
+        // 기본타입 8종류
+        // 정수타입 - byte, char, short, int, long
+        // 실수 타입 - float, double
+        // 논리 타입 - boolean
+
+        // char 타입
+        // 문자 리터럴이라고 하며 유니코드로 변환되어 저장.
+        // 작은따옴표 ('')로 감쌈
+        char var1 = 'A';    // 유니코드 : 65
+
+        // String 타입
+        //
+
+
+    }
+}
