b1ce270b93521a43d767f3e5282db872ba92de9e
dwng3 <answer3dw@gmail.com>
2023-04-19 21:21:39 +0900

    혼공자 챕터3

---
+++ Study/src/SoloJava/Chapter03/ex01_연산자와연산식.java
@@ -0,0 +1,11 @@
+package SoloJava.Chapter03;
+
+public class ex01_연산자와연산식 {
+    public static void main(String[] args) {
+
+
+
+
+
+    }
+}
---
+++ Study/src/SoloJava/Chapter03/ex02_연산자의종류.java
@@ -0,0 +1,17 @@
+package SoloJava.Chapter03;
+
+public class ex02_연산자의종류 {
+    public static void main(String[] args) {
+
+        // 1. 삼항 연산자
+        // 조건 연산식이라고 부르기도 함
+
+        int score = 95;
+        char grade = (score>90)? 'A' : 'B';
+        System.out.println(grade);
+        // 출력 A
+
+
+
+    }
+}
